a1a79514f7fa4de0ff9738facffd0595ed1be729
poro <40479447+poro912@users.noreply.github.com>
2023-09-04 20:16:00 +0900

    cerate class

--- Module/Interface/Interface/Card.cs
+++ Module/Interface/Interface/Card.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interface
+namespace TaroInterface
 {
-	internal class Class1
+	internal class Card
 	{
+		private int number = -1;
+		private string name = "";
+		private string category = "";
+
+		
+		public int Number{ get; set; }
+		public int Name { get; set; }
+		public int Category { get; set; }
+
+		public List<string> forward;
+		public List<string> reverse;
+
+		public Card(string name, int number) 
+		{ 
+			this.name = name;
+			this.number = number;
+			forward = new List<string>();
+			reverse = new List<string>();
+		}
+		public Card(string name, int number, string category)
+		{
+			this.name = name;
+			this.number = number;
+			this.category = category;
+			forward = new List<string>();
+			reverse = new List<string>();
+		}
+
+		public void addForwardMean(string mean) { forward.Add(mean);  }
+		public void addFrontMean(string mean) { forward.Add(mean); }
+
 	}
 }
---
+++ Module/Interface/Interface/Slot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaroInterface
+{
+	internal class Slot
+	{
+		private int x, y;
+		private Card? card;
+		private string? mean;
+
+		public int X { get; set; }
+		public int Y { get; set; }
+		public string? Mean { get; set; }
+		public Card? Card { get { return card; } }
+		
+		public bool registCard(Card card)
+		{
+			this.card = card;
+			return true;
+		}
+
+		public bool removeCard()
+		{
+			if(this.card == null)
+				return false; 
+			
+			this.card = null;
+			return true;
+		}
+	}
+}
