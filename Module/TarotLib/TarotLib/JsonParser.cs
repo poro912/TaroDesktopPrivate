ba01837aa1b4c2ac7f93c2e15fc85a89eaf3e4f0
poro <40479447+poro912@users.noreply.github.com>
2023-09-13 10:04:41 +0900

    update jsonParser

--- Module/TarotLib/TarotLib/JsonParser.cs
+++ Module/TarotLib/TarotLib/JsonParser.cs
@@ -1,6 +1,24 @@
-﻿namespace TarotLib
+﻿using TarotLib.Models;
+
+namespace TarotLib
 {
-    public class JsonParser
-    {
-    }
+	public class JsonParser
+	{
+		public JsonParser() { }
+
+		public FileStream? openJson(string path)
+		{
+			return null;
+		}
+		
+		public List<Card>? readCards()
+		{
+			return null;
+		}
+
+		public List<Card>? readSpread()
+		{
+			return null;
+		}
+	}
 }
